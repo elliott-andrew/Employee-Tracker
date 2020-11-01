7c0489db1ef8d31cf84eaf4cff21b1c602dc02a0
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-10-31 19:04:03 -0600

    Imported ASCII art npm package. Added logo and mini bio to beginning of start. Added first question

---
+++ index.js
@@ -0,0 +1,23 @@
+const figlet = require('figlet');
+const inquirer = require('inquirer');
+
+figlet('Employee\nSummary', function (err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data + '\n' + '\nAdd departments, roles and employess within your company\nto easily organize and your plan your business.\n' + '\n' + 'Begin questions:' + '\n')
+    questions();
+});
+
+const questions = () => {
+    inquirer
+        .prompt([
+            {
+                name: 'EmployeeName',
+                message: 'What is the employee\'s name?',
+                type: 'input'
+            }
+        ]);
+};
