72e229ca8a64e8bf384927e626715eb02c47a320
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-11-01 15:47:14 -0700

    Created select function to return all employees with their assigned departments, employee id numbers, salaries and managers

--- EmployeeSummary-Schema.sql
+++ EmployeeSummary-Schema.sql
@@ -8,17 +8,9 @@ CREATE TABLE department (
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
-
-CREATE TABLE employee (
-  id INT AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT,
-  manager_id INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (manager_id) REFERENCES department(id),
-  FOREIGN KEY (role_id) REFERENCES role(id)
-);
+INSERT INTO department VALUES (1, 'Men\'s Clothing');
+INSERT INTO department VALUES (2, 'Women\'s Clothing');
+INSERT INTO department VALUES (3, 'Toy Department');
 
 CREATE TABLE role (
   id INT AUTO_INCREMENT,
@@ -28,8 +20,40 @@ CREATE TABLE role (
   PRIMARY KEY (id),
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
+INSERT INTO role VALUES (1, 'Manager', 200000.00, 1);
+INSERT INTO role VALUES (2, 'Manager', 300000.00, 2);
+INSERT INTO role VALUES (3, 'Cashier', 45000.00, 1);
+INSERT INTO role VALUES (4, 'Cashier', 54000.00, 2);
 
-SELECT * FROM employee;
-SELECT * FROM department;
-SELECT * FROM role;
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (manager_id) REFERENCES role(id),
+  FOREIGN KEY (role_id) REFERENCES role(id)
+);
+INSERT INTO employee VALUES (1, 'Harris', 'Milstead', 1, null);
+INSERT INTO employee VALUES (2, 'John', 'Waters', 2, null);
+INSERT INTO employee VALUES (3, 'Hem', 'The Cat', 3, 1);
+INSERT INTO employee VALUES (4, 'Kyle', 'The Cat', 4, 2);
 
+-- SELECT * FROM department;
+-- SELECT * FROM role;
+-- SELECT * FROM employee;
+
+SELECT 
+d.name,
+r.title,
+e.role_id,
+e.first_name,
+e.last_name,
+r.salary,
+e.manager_id
+FROM department d
+JOIN role r
+	ON d.id = r.department_id
+JOIN employee e
+	ON e.role_id = r.id
